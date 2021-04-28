aa81db7968202332664f7648bff75183e44b8372
debrax <leonard.beck@gmail.com>
2021-04-28 20:04:23 +0200

    updaters

--- gilded-rose/GildedRose/GildedRose.cs
+++ gilded-rose/GildedRose/GildedRose.cs
@@ -21,65 +21,23 @@ namespace GildedRose
 
         private void UpdateItem(Item item)
         {
+            ItemUpdater itemUpdater;
             switch (item.Name)
             {
                 case "Aged Brie":
-                    var updater = new BrieUpdater();
-                    updater.UpdateItem(item);
+                    itemUpdater = new BrieUpdater();
                     break;
                 case "Backstage passes to a TAFKAL80ETC concert":
-                    IncreaseQuality(item);
-                    if (item.SellIn < 11)
-                    {
-                        IncreaseQuality(item);
-                    }
-                    if (item.SellIn < 6)
-                    {
-                        IncreaseQuality(item);
-                    }
-                    DecreaseSellIn(item);
-                    if (item.SellIn < 0)
-                    {
-                        ResetQuality(item);
-                    }
+                    itemUpdater = new BackstagePassesUpdater();
                     break;
                 case "Sulfuras, Hand of Ragnaros":
+                    itemUpdater = new SulfurasUpdater();
                     break;
                 default:
-                    DecreaseQuality(item);
-                    DecreaseSellIn(item);
-                    if (item.SellIn < 0)
-                    {
-                        DecreaseQuality(item);
-                    }
+                    itemUpdater = new DefaultItemUpdater();
                     break;
             }
-        }
-
-        private static void ResetQuality(Item item)
-        {
-            item.Quality = 0;
-        }
-
-        private static void IncreaseQuality(Item item)
-        {
-            if (item.Quality < 50)
-            {
-                item.Quality++;
-            }
-        }
-
-        private static void DecreaseQuality(Item item)
-        {
-            if (item.Quality > 0)
-            {
-                item.Quality--;
-            }
-        }
-
-        private static void DecreaseSellIn(Item item)
-        {
-            item.SellIn--;
+            itemUpdater.UpdateItem(item);
         }
     }
 }
--- gilded-rose/GildedRose/ItemUpdater.cs
+++ gilded-rose/GildedRose/ItemUpdater.cs
@@ -1,28 +1,83 @@
 ﻿namespace GildedRose
 {
-    public class BrieUpdater
+    public abstract class ItemUpdater
     {
-        public void UpdateItem(Item item)
-        {
-            DecreaseSellIn(item);
+        public abstract void UpdateItem(Item item);
 
-            IncreaseQuality(item);
-            
-            if (item.SellIn < 0)
-                IncreaseQuality(item);
-        }
-
-        private static void DecreaseSellIn(Item item)
+        protected static void DecreaseSellIn(Item item)
         {
             item.SellIn--;
         }
 
-        private static void IncreaseQuality(Item item)
+        protected static void IncreaseQuality(Item item)
         {
             if (item.Quality < 50)
-            {
                 item.Quality++;
+        }
+    }
+
+    public class BrieUpdater : ItemUpdater
+    {
+        public override void UpdateItem(Item item)
+        {
+            DecreaseSellIn(item);
+
+            IncreaseQuality(item);
+
+            if (item.SellIn < 0)
+                IncreaseQuality(item);
+        }
+    }
+
+    public class BackstagePassesUpdater : ItemUpdater
+    {
+        public override void UpdateItem(Item item)
+        {
+            IncreaseQuality(item);
+            if (item.SellIn < 11)
+            {
+                IncreaseQuality(item);
+            }
+            if (item.SellIn < 6)
+            {
+                IncreaseQuality(item);
+            }
+            DecreaseSellIn(item);
+            if (item.SellIn < 0)
+            {
+                ResetQuality(item);
             }
         }
+
+        private static void ResetQuality(Item item)
+        {
+            item.Quality = 0;
+        }
+    }
+
+    public class DefaultItemUpdater : ItemUpdater
+    {
+        public override void UpdateItem(Item item)
+        {
+            DecreaseQuality(item);
+            DecreaseSellIn(item);
+            if (item.SellIn < 0)
+            {
+                DecreaseQuality(item);
+            }
+        }
+
+        private static void DecreaseQuality(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality--;
+            }
+        }
+    }
+
+    public class SulfurasUpdater : ItemUpdater
+    {
+        public override void UpdateItem(Item item) { }
     }
 }
