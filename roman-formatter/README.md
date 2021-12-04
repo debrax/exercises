62982087363d2a58f00c63f013124af0cd6740d1
debrax <leonard.beck@gmail.com>
2021-12-04 02:07:58 +0100

    feat(score-keeper) (#3)

--- roman-formatter/README.md
+++ roman-formatter/README.md
@@ -1,12 +1,14 @@
 # Roman Formatter
 
-Integer formatting into roman notation (C#).  
-`RomanFormatter.ToRomanNotation(1945); // "MCMXLV"`
+Original kata on [Kata-Log](https://kata-log.rocks/roman-numerals-kata).  
+Topics: C#, TDD  
+**Try it before looking at the solution!**
 
-*Personal solution after coding kata with colleagues (TDD and VS Code tooling).*
+## Requirements
 
-## Setup
-
-- .NET Core 3.1 SDK
-- `dotnet build`
-- `dotnet test`
+> Create a program which formats a year (integer) into roman notation:
+> `RomanFormatter.ToRomanNotation(1945); // "MCMXLV"`
+> - I (1), X (10) and C (100) up to 4 times
+> - V (1), L (50) max once
+> - M (1000) unlimited
+> - IV (4), XL (40), CM (900) max once
---
+++ score-keeper/ScoreKeeper/ScoreKeeper.cs
@@ -0,0 +1,26 @@
+﻿namespace ScoreKeeper
+{
+    public class ScoreKeeper
+    {
+        private TeamScore scoreA = new TeamScore();
+        private TeamScore scoreB = new TeamScore();
+
+        public string GetScore() => $"{scoreA}:{scoreB}";
+
+        public void ScoreTeamA1() => scoreA.AddPoints(1);
+        public void ScoreTeamA2() => scoreA.AddPoints(2);
+        public void ScoreTeamA3() => scoreA.AddPoints(3);
+
+        public void ScoreTeamB1() => scoreB.AddPoints(1);
+        public void ScoreTeamB2() => scoreB.AddPoints(2);
+        public void ScoreTeamB3() => scoreB.AddPoints(3);
+
+        private class TeamScore
+        {
+            private int score = 0;
+
+            public void AddPoints(int points) => score += points;
+            public override string ToString() => $"{score:000}";
+        }
+    }
+}
